6832d3420e4921e48672d765a7db3b03ea292192
N.Maurice <cirilaii.nm@gmail.com>
2023-12-14 16:30:01 +0100

    fin mise en page formulaire commande

--- wp-content/themes/planty_themeChild/functions.php
+++ wp-content/themes/planty_themeChild/functions.php
@@ -10,8 +10,7 @@ function theme_enqueue_styles(){
     // Chargement du css/theme.css pour nos personnalisations
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', 
     array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/responsive.css', 
-    array(), filemtime(get_stylesheet_directory() . '/css/responsive.css'));
+   
 
 }
 
@@ -19,13 +18,16 @@ function childtheme_supports() {
     register_nav_menu('footer', 'Pied de page');
 }
 // affichage de la page Admin dans le menu
-// function($items, $args){
-//     if(is_user_logged_in() && $args->menu == main-menu){
-//         $adminItemMenu = '<li id="menu-item-111" class="menu-item">admin</li>'<li id="menu-item-112" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-56 current_page_item menu-item-112"><a href="http://planty/?page_id=56" aria-current="page" itemprop="url"><span itemprop="name">Nous rencontrer</span></a></li>;
-//         $items = $adminItemMenu
-//     }
-//     return $items;
-// };
-// add_filter('wp_nav_menu_items', 'add_admin_menu_item', 10, 2);
+add_filter('wp_nav_menu_items', 'add_admin_menu_item', 10, 2);
+function add_admin_menu_item($items, $args){
+    if(is_user_logged_in() && $args->theme_location == 'main-menu'){
+        $adminItemMenu = '<li id="menu-item-111" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-111">
+        <a href="http://planty/wp-admin" aria-current="page" itemprop="url">
+        <span itemprop="name">admin</span></a></li>';
+        $items .= $adminItemMenu;
+    }
+    return $items;
+};
+
 
 ?>
